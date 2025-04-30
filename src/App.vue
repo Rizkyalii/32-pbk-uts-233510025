d1293c9cf8c2dcc0217a19356f23d05ac164db13
Rizkyalii <ikymlnaali09@gmail.com>
2025-04-30 01:07:12 -0700

    Pembuatan ADD kegiatan

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, computed } from "vue";
+
+const activity = ref("");
+
+const data = ref([]);
+
+</script>
+
+<template>
+  <div id="app">    
+    <div class="addData">
+      <input v-model="activity" placeholder="Tambahkan Kegiatan" />
+      <button @click="addData">Submit</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

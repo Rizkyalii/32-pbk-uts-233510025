89c8d9a3164fb0e387cbff5a732882670c4d7e7e
Rizkyalii <ikymlnaali09@gmail.com>
2025-04-30 01:14:47 -0700

    Menambahkan Filtering Data kegiatan

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,33 @@ const activity = ref("");
 
 const data = ref([]);
 
+const dataIncomplete = ref(false);
+
+const addData = () => {
+  data.value.push({ text: activity.value, done: false });
+  activity.value = "";
+};
+
+const filterData = computed(() => {
+  return dataIncomplete.value ? data.value.filter((todo) => !todo.done) : data.value;
+});
 </script>
 
 <template>
-  <div id="app">    
+  <div id="app">
     <div class="addData">
       <input v-model="activity" placeholder="Tambahkan Kegiatan" />
       <button @click="addData">Submit</button>
     </div>
+
+    <div class="dataNotReady">
+      <label>
+        <input type="checkbox" v-model="dataIncomplete" />
+        Tampilkan kegiatan yang belum selesai
+      </label>
+    </div>
+
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
